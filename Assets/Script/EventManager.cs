0711010a3a71f32723f902088031151c5bf980e5
aman <gr030315@gmail.com>
2023-12-09 16:38:32 +0900

    EventManager 주석 완료

--- Assets/Script/EventManager.cs
+++ Assets/Script/EventManager.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 
 public class EventManager : MonoBehaviour
 {
-    private static EventManager _instance;  
+    private static EventManager _instance;  // 싱글톤
     public static EventManager Instance => _instance;
     public void Awake()
     {
-        if (_instance == null)
+        if (_instance == null) // 싱글톤 등록
         {
             _instance = this;
             DontDestroyOnLoad(gameObject);
@@ -17,34 +17,43 @@ public class EventManager : MonoBehaviour
         else Destroy(this);
     }
 
-    public delegate void Event();
-    public delegate GameObject GOEvent();
+    //클래스 간의 소통을 위한 Event들 : 클래스 멤버 변수에 변화가 발생하거나 입력이 들어오면 해당 정보를 다른 클래스로 전달
 
-    public Action CreateItem;
+    // 플레이어 입력에 대한 반응 함수
+
+    // Item 신규 생성
+    public Action CreateItem; 
+    // 플레이어 꼬리 추가
     public Action<GameObject> AddTail;
+    // 게임 오버
     public Action GameOver;
+    // 최고 점수 업데이트
     public Action<int> UpdateScore;
+    //꼬리 프리팹 생성
     public Func<GameObject> CreateTail;
+    // 플레이어 
     public Func<Transform> GetHeadPos;
+    // 최고 점수 가져오기
     public Func<int> GetHighestScore;
+
+
+    //이벤트 호출
     public void CallOnCreateItem()
     {
         CreateItem?.Invoke();
     }
-
     public GameObject CallOnCreateTail()
     {
         return CreateTail?.Invoke();
+    }
+    public void CallOnGameOver()
+    {
+        GameOver?.Invoke();
     }
 
     public void CallOnAddTail(GameObject tail)
     {
         AddTail?.Invoke(tail);
-    }
-
-    public void CallOnGameOver()
-    {
-        GameOver?.Invoke();
     }
 
     public Transform CallOnGetHeadPos()
@@ -61,8 +70,5 @@ public class EventManager : MonoBehaviour
     {
         return (int)(GetHighestScore?.Invoke());
     }
-
-
-
 
 }
